b50e0f5364d6f0d63716ae8f90553005970cf40f
Tania Filonenko <Tania.Filonenko@gmail.com>
2015-07-25 00:19:00 +0300

    improve code

--- tmp21/sandbox21/ruby_bursa_task_3/book.rb
+++
@@ -1,9 +0,0 @@
-class Book
-  attr_accessor :author, :title
-
-  def initialize author, title
-    @author = author
-    @title = title
-  end
-
-end
--- tmp9/sandbox9/ruby_bursa_task_3-master/reader.rb
+++
@@ -1,9 +0,0 @@
-class Reader
-  attr_accessor :name, :reading_speed
-
-  def initialize name, reading_speed
-    @name = name
-    @reading_speed = reading_speed
-  end
-
-end
